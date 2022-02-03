51bf481051f7cb2a59608484328b94fd23256ef3
Fernanda Javi3ra <javi3ra20@gmail.com>
2022-02-03 18:54:53 -0300

    edit login

--- src/components/layout/MainLayout/LoginLayout/index.jsx
+++ src/components/layout/MainLayout/LoginLayout/index.jsx
@@ -1,14 +1,10 @@
-import logoNest from '../../../../Image/nest2.png'
 
 
 export default function LoginLayout({ children }) {
     return (
 
         <div className='login'>    
-            <div className="titleLogin">
-            <div>
-            <img src={logoNest} />
-            </div>
+            <div className="titleLogin">         
                 <h1> Bienvenido a NEST</h1>
                 <p /><h4> Portal de alumnos en línea</h4>
             </div>  
